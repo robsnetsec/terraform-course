3567580447c1c1604b17122f11793805ec3cdbf0
Robby <robrang558@hotmail.com>
2023-02-02 16:41:29 -0600

    demo3

--- demo-3/vars.tf
+++ demo-3/vars.tf
@@ -5,15 +5,15 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1 = "ami-0778521d914d23bc1"
+    us-west-2 = "ami-0c4dfe348469b0ae5"
+    eu-west-1 = "ami-0d729a60"
   }
 }
 
